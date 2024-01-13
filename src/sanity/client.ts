7d5a589ef9f5d8ef8104db6e117f77d20b2d00d8
Roberto Loss <robertoloss@gmail.com>
2024-01-13 17:29:26 -0500

    minor fix

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,4 +1,4 @@
-import {createClient} from '@sanity/client'
+import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 import { Picture } from './sanity-types'
 
