d8504af70e603a4c197c8397e1505183ae829a9e
Roberto Loss <robertoloss@gmail.com>
2024-01-13 16:10:06 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "./components/ThemeProvider"
+import { ModeToggle } from "./components/ModeToggle"
+
+
+function App() {
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<ModeToggle/>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'i3j6s8ke',
+    dataset: 'production'
+  }
+})
