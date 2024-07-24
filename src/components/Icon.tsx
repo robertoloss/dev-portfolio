c04f6acf5354bd90d6ba80a32b66a2d37bbf5b59
Roberto Loss <robertoloss@gmail.com>
2024-07-24 15:48:42 -0400

    icons fixed

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -18,10 +18,16 @@ export default function Icon({ icon, index, card } : Props) {
 					transition-[margin] duration-100 ease-linear cursor-pointer 
 					${!card && (icon.name === 'rust' || icon.name === 'react-router') ? 
 					' dark:bg-gray-800' : ''}
-					${icon.name == 'bubble' || icon.name == 'rust' ? !card ? 'dark:bg-gray-600' : 'dark:bg-gray-400' : 
-						card ? 'dark:bg-muted bg-border' : 'bg-background'}
+					${icon.name == 'bubble' || icon.name == 'rust' 
+						? !card 
+							? 'bg-background dark:bg-gray-600' 
+							: 'bg-border dark:bg-gray-400' 
+						: card 
+							? 'dark:bg-muted bg-border' 
+							: 'bg-background'
+						}
 					`}
-					>
+				>
 						{icon.name?.toLowerCase() != 'sanity' && 
 							<img alt={icon.alt!} src={urlFor(icon.image!)!.width(32).url()} />}
 						{icon.name?.toLowerCase() === 'sanity' && <SanitySVG />}
